4479ae8937ab32e564aff15f51bf80cd967a8fdd
nakam t <dribble0912@gmail.com>
2016-03-06 10:45:15 +0900

    commit

--- web/views.py
+++ web/views.py
@@ -35,7 +35,7 @@ def ajaxdataview(req):
 
 
 def getCurrencyDBData(reqbid=None):
-    queryset = Sensor.objects.order_by("-timestamp")[:15]
+    queryset = Sensor.objects.order_by("-timestamp")[0:25]
     return queryset
 
 
